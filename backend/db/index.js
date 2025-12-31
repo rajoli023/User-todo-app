8c8e34b78831a8928b1d5be1af2c4731f8db7459
Raj Oli <rajoli2025@gmail.com>
2025-12-31 00:13:30 -0500

    Initial backend setup

---
+++ backend/db/index.js
@@ -0,0 +1,22 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
+
+db.getConnection((err, connection) => {
+  if (err) {
+    console.error(" MySQL connection failed:", err.message);
+  } else {
+    console.log(" MySQL connected successfully");
+    connection.release();
+  }
+});
+
+module.exports = db;
+
