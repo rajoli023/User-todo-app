ed508936f3abd01d62aecbf23b496be955d6bcfc
Raj Oli <rajoli2025@gmail.com>
2026-01-08 13:40:05 -0500

    Add login and JWT auhenctication

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,13 @@
 
 require("dotenv").config();
 const express = require("express");
-
+const userRoutes = require("./routes/userRoutes");
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 app.use(express.json());
-
 app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running");
--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,7 @@
 const db = require("../db/db");
 const bcrypt = require("bcrypt");
-
+const jwt = require("jsonwebtoken");
+// Register User
 exports.registerUser = async (req, res) => {
     
     const { username, email, password } = req.body;
@@ -29,4 +30,39 @@ exports.registerUser = async (req, res) => {
     }
 };
 
+// Login User
+exports.loginUser = async (req, res) => {
+    const {email, password} = req.body;
 
+    if (!email || !password) {
+        return res.status(400).json({message: "All Fields are required"});
+
+    }
+    try {
+        const [users] = await db.query("SELECT * FROM users WHERE email = ?", [email]);
+
+        if (users.length === 0) {
+            return res.status(401).json({message: "Invalid email or password"});
+        }
+        const user = users[0];
+
+        const isMatch = await bcrypt.compare(password, user.password_hash);
+        
+        if (!isMatch) {return res.status(401).json ({message : "Invalid email or password"});
+     }
+
+    // Geneterate JWT Token
+    const token = jwt.sign(
+        {id: user.id, email : user.email},
+        process.env.JWT_SECRET,
+        {expiresIn: "1d"}
+
+    );
+    res.json({
+        message : " Login successfull",
+        token,
+    });
+    }catch (error) {
+        res.status(500).json({message : "Server Error ", error : error.message});
+    }
+};
